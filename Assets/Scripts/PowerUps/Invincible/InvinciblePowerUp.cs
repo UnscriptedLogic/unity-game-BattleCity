ceb5efcf5b3123d7535d8b4d9c588a625491a44e
Nathaniel <semblancewarden@Gmail.com>
2022-03-03 19:46:33 +0800

    Power Ups (Invincibility)
    - converted [Forcefield] into [Invincibility], completely rendering you immune from all types of harm instead of just shielding you.

--- Assets/Scripts/PowerUps/Invincible/InvinciblePowerUp.cs
+++ Assets/Scripts/PowerUps/Invincible/InvinciblePowerUp.cs
@@ -6,15 +6,42 @@ public class InvinciblePowerUp : PowerUpManager
 {
     public float duration;
     public GameObject forcefieldPrefab;
+    private BoxCollider boxCollider;
 
     protected override void Activate(Collision collision)
     {
-        GameObject forcefield = Instantiate(forcefieldPrefab, collision.transform);
-        Invincible invincibleScript = forcefield.GetComponent<Invincible>();
-        invincibleScript.duration = duration;
-        invincibleScript.shieldOwner = collectorManager;
+        boxCollider = collectorManager.GetComponent<BoxCollider>();
 
+        //Create a gameobject to house the fake collider
+        GameObject fakeColliderGO = Instantiate(forcefieldPrefab, collectorManager.transform);
+        fakeColliderGO.transform.position = collectorManager.transform.position;
+        fakeColliderGO.transform.localScale = Vector3.one;
+
+        //Create a fake collider - damage scripts will look for an entityhealth. Seeing there's none, no damage done
+        BoxCollider fakeBoxCollider = fakeColliderGO.AddComponent<BoxCollider>();
+        fakeBoxCollider.size = boxCollider.size;
+        fakeBoxCollider.center = boxCollider.center;
+        fakeBoxCollider.isTrigger = false;
+
+        //Adds the duration script and what to do when the script duration runs out
+        Invincible invincible = collectorManager.gameObject.AddComponent<Invincible>();
+        invincible.fakeColliderGO = fakeColliderGO;
+        invincible.duration = duration;
+        invincible.realCollder = boxCollider;
+
+        //Sound effects and destroy
         base.Activate(collision);
         SelfDestruct();
     }
+
+    //protected override void Activate(Collision collision)
+    //{
+    //    GameObject forcefield = Instantiate(forcefieldPrefab, collision.transform);
+    //    Invincible invincibleScript = forcefield.GetComponent<Invincible>();
+    //    invincibleScript.duration = duration;
+    //    invincibleScript.shieldOwner = collectorManager;
+
+    //    base.Activate(collision);
+    //    SelfDestruct();
+    //}
 }
