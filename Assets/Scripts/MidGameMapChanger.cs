a1f3e4840c21ef57aebe8cbe40bed4efa0d37176
Nathaniel <semblancewarden@Gmail.com>
2022-04-10 17:46:08 +0800

    Map Cycling and Sniper Tank shooting sound effects

--- Assets/Scripts/MidGameMapChanger.cs
+++ Assets/Scripts/MidGameMapChanger.cs
@@ -2,9 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
-public class MidGameMapChanger : MonoBehaviour
+public class MidGameMapChanger : Semaphore
 {
+    [SerializeField] private PointShopWaveSpawner spawner;
+    [SerializeField] private int wavesToChange;
+    [SerializeField] private Vector2Int mapBuildIndexRange;
+    private int waves;
+    private int mapIndex;
 
+    protected override void SephamoreStart(Manager manager)
+    {
+        mapIndex = SceneManager.GetSceneAt(1).buildIndex;
+        spawner.onWaveStarting += OnWaveCompleted;
+    }
+
+    public void OnWaveCompleted()
+    {
+        waves++;
+        if (waves >= wavesToChange)
+        {
+            mapIndex++;
+            if (mapIndex > mapBuildIndexRange.y)
+            {
+                mapIndex = mapBuildIndexRange.x;
+            }
+
+            SceneManager.LoadSceneAsync(mapIndex, LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(1));
+            waves = 0;
+        }
+    }
 }
